8bb0cb4fbb1e26a8f2a045abbc4e40f148308343
Abhishek Sunil <abhisheksunil92@gmail.com>
2022-07-08 09:50:39 +0530

    trying to fix cors

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-const { ApolloServer, PubSub } = require("apollo-server");
+const { PubSub } = require("apollo-server");
+const { ApolloServer } = require("apollo-server-express");
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 
+require("dotenv").config();
+
 const pubsub = new PubSub();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
-app.use(
-  cors({
-    origin: "https://peoplemedia.netlify.app/",
-    credentials: true,
-    optionSuccessStatus: 200,
-  })
-);
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: ({ req }) => ({ req, pubsub }),
-});
-
-server.applyMiddleware({ app });
-
-mongoose
-  .connect(process.env.MONGODB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("MongoDB Connected");
-    return server.listen({ port: PORT });
-  })
-  .then((res) => {
-    console.log(`Server running at ${res.url}`);
-  })
-  .catch((err) => {
-    console.log(err);
+const startApolloServer = async () => {
+  const app = express();
+  app.use(cors());
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => ({ req, pubsub }),
   });
+
+  await server.start();
+  server.applyMiddleware({ app });
+
+  mongoose
+    .connect(process.env.MONGODB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("MongoDB Connected");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  await new Promise((resolve) => app.listen({ port: PORT }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+};
+
+startApolloServer();
